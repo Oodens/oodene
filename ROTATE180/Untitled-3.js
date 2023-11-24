73bcd5f8f8e300ed1e8f1d5d4b72bbf3c4867c07
Oodens <151909600+Oodens@users.noreply.github.com>
2023-11-24 12:34:42 +0500

    Add files via upload

---
+++ ROTATE180/Untitled-3.js
@@ -0,0 +1,17 @@
+/* let a = 32;
+let b = 32;
+if (a > b){
+    console.log('1 число > 2 число')
+}
+else if(a < b){
+    console.log('1 число < 2 число ')
+}
+else {console.log('1 число = 2 число')} */
+
+let c = parseInt(window.prompt('число ='))
+let z = 50;
+let x = '50';
+if (c % 2 == 0 ){
+    console.log('четное')
+}
+else { console.log('нечетное')}
